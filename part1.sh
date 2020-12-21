b323ed91947fdaf663cef310a83c4044ce8f5da6
zbenii <besic.beni@gmx.at>
2020-12-21 17:32:09 +0100

    fixed part1.sh and gh pages

--- part1.sh
+++ part1.sh
@@ -3,8 +3,10 @@ BUILD_DIR="docs"
 rm -rf -v $BUILD_DIR;
 
 mkdir -p docs;
+mkdir -p docs/images;
 
 cp -r -p -v asciidocs/docinfo.html $BUILD_DIR;
 
 cp -r -p -v asciidocs/*.adoc $BUILD_DIR;
 
+cp -r -p -v asciidocs/images/* $BUILD_DIR/images;
